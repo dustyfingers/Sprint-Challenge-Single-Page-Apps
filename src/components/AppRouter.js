eb3304736e73b8fa40d0528b20f4c03d3d5482d7
dustyfingers <dustyfingers01257@gmail.com>
2019-07-26 11:39:08 -0600

    finished mvp

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
-import CharacterList from './CharacterList'
 import WelcomePage from './WelcomePage';
+import CharacterList from './CharacterList'
 import LocationsList from './LocationsList';
+import EpisodeList from './EpisodeList';
 
 export default function AppRouter() {
   return <div className="page-view ui bottom attached segment active tab">
     <Switch>
       <Route path='/characters' component={CharacterList} />
       <Route path='/locations' component={LocationsList} />
-      <Route component={WelcomePage} />
-      {/* <Route path="/episodes" component={} /> */}
+      <Route path="/episodes" component={EpisodeList} />
+      <Route exact path="/" component={WelcomePage} />
+
     </Switch>
   </div>
 
